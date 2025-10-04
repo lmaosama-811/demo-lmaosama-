f0127c057d802c5c4747ac7ab627c4275763ebf6
Bach Ha <havietbach.tq@gmail.com>
2025-10-05 06:41:03 +0700

    update more

--- testing2.py
+++ testing2.py
@@ -1,2 +1,3 @@
 print("github is such a dick, ridiculous")
-print('However, it's pretty fun when you know how to use it")
+print("However, it's pretty fun when you know how to use it")
+print("but it's too hard to master")
