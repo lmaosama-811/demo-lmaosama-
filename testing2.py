387e64a30e93ed4a371393c9db81db74a96c7cd6
lmaosama-811 <havietbach.tq@gmail.com>
2025-10-05 08:17:27 +0700

    Update testing2.py

--- testing2.py
+++ testing2.py
@@ -3,4 +3,4 @@ print("However, it's pretty fun when you know how to use it")
 print("but it's too hard to master")
 print("and I don't like it")
 print("but it's too hard to master")
-
+print("create conflict in purpose")
